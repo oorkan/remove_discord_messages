395796b259c49616c7a41f98b0b64087c5769e18
oorkan <oorkan.dev@gmail.com>
2020-07-01 07:28:53 +0400

    create  function

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 // Needs to be refactored
 // For now, just a trash script that works 
 
-userLogin = 'Paste your username here';
-moreText = 'Paste Edit Message -> More button text here in the language you use';
-scroller = document.querySelector('.messagesWrapper-3lZDfY .scroller-2FKFPG');
-interval = setInterval(function() {
-    document.querySelectorAll(".message-2qnXI6").forEach(function(value) {
-        value.dispatchEvent(new MouseEvent('mousemove', { 'bubbles': true }));
-    });
-    username = document.querySelectorAll('.header-23xsNx .username-1A8OIy');
-    username.forEach(function(value, index, obj) {
-        let username = value.innerText;
-        if(username == userLogin) {
-            let message_container = value.closest('.message-2qnXI6');
-            if(message_container) {   
-                message_container.click();
-                setTimeout(function() {
-                    let more = message_container.querySelector('.button-1ZiXG9[aria-label='+moreText+']');
-                    console.log(more);
-                    if(more) {
-                        more.click();
-                        let message_actions_delete = document.querySelector("#message-actions #message-actions-delete");
-                        if(message_actions_delete) {
-                            message_actions_delete.click();
-                            setTimeout(function() {
-                                let mad_btn = document.querySelector(".modal-3c3bKg .button-38aScr[type=submit]");
-                                mad_btn.click();
-                            }, 200);            
+const remove_messages = (username, more_btn) => {
+    let scroller = document.querySelector('.messagesWrapper-3lZDfY .scroller-2FKFPG'),
+        interval = setInterval(function() {
+        let username_container = document.querySelectorAll('.header-23xsNx .username-1A8OIy');
+        
+        document.querySelectorAll(".message-2qnXI6").forEach(function(value) {
+            value.dispatchEvent(new MouseEvent('mousemove', { 'bubbles': true }));
+        });
+        
+        username_container.forEach(function(value, index, obj) {
+            let me = value.innerText;
+            
+            if(me == username) {
+                let message_container = value.closest('.message-2qnXI6');
+                
+                if(message_container) {   
+                    message_container.click();
+                    setTimeout(function() {
+                        let more = message_container.querySelector('.button-1ZiXG9[aria-label='+more_btn+']');
+                        
+                        if(more) {
+                            more.click();
+                            let message_actions_delete = document.querySelector("#message-actions #message-actions-delete");
+                            if(message_actions_delete) {
+                                message_actions_delete.click();
+                                setTimeout(function() {
+                                    let mad_btn = document.querySelector(".modal-3c3bKg .button-38aScr[type=submit]");
+                                    mad_btn.click();
+                                }, 200);            
+                            }
                         }
-                    }
-                }, 200);
+                    }, 200);
+                }
+                else {
+                    setTimeout(function() {
+                            scroller.scroll(0,0);
+                    }, 3000)
+                }
             }
-            else {
-            	setTimeout(function() {
-            			scroller.scroll(0,0);
-            	}, 3000)
-            }
-        }
-    });
-}, 600);
+        });
+    }, 600);
+}
+
+remove_messages(
+    'Paste your username here',
+    'Paste Edit Message -> More button text here in the language you use'
+);
